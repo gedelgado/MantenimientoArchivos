81615297dceb71f4759dcf77a974d54c3042caa3
Ge Delgado <Ge Delgado@DESKTOP-N7C0O09>
2021-11-10 21:29:39 -0600

    Add project files.

---
+++ MantenimientoArchivos/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace MantenimientoArchivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(294, 172);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(294, 143);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(294, 114);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 2;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cédula:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Correo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dirección:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Salario:";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(72, 20);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 20);
+            this.txtCedula.TabIndex = 8;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(72, 50);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 9;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(72, 79);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(100, 20);
+            this.txtCorreo.TabIndex = 10;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(72, 109);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtDireccion.TabIndex = 11;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(72, 138);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 12;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(294, 85);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 13;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Enabled = false;
+            this.btnBorrar.Location = new System.Drawing.Point(294, 27);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 14;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Enabled = false;
+            this.btnModificar.Location = new System.Drawing.Point(294, 56);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 15;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 209);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnSalir);
+            this.MaximumSize = new System.Drawing.Size(426, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnModificar;
+    }
+}
+
---
+++ MantenimientoArchivos/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace MantenimientoArchivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void Limpiar()
+        {
+            txtCedula.Clear();
+            txtCorreo.Clear();
+            txtDireccion.Clear();
+            txtNombre.Clear();
+            txtSalario.Clear();
+            txtCedula.Focus();
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtCedula.Text != "" && txtCorreo.Text != "" && txtDireccion.Text != "" && txtNombre.Text != "" && txtSalario.Text != "")
+
+                try
+                {
+
+
+                    StreamWriter arch = null;
+                    //OTRAS FORMAS DE CREAR LOS ARCHICOS
+                    //arch= File.AppendText("Datos.txt");
+                    //arch.WriteLine(txtCedula.Text);
+                    // arch.WriteLine(txtNombre.Text);
+                    //arch.WriteLine(txtCorreo.Text);
+                    //arch.WriteLine(txtDireccion.Text);
+                    //arch.WriteLine(txtSalario.Text);
+                    //arch.Close();
+                    //Limpiar();
+
+
+                    arch = File.AppendText("Datos.txt"); //PONER LA RUTA CORRECTA 
+                    arch.Write(txtCedula.Text + "#");
+                    arch.Write(txtNombre.Text + "#");
+                    arch.Write(txtCorreo.Text + "#");
+                    arch.Write(txtDireccion.Text + "#");
+                    arch.WriteLine(txtSalario.Text);
+                    arch.Close();
+                    Limpiar();
+                    MessageBox.Show("Registro ingresado con exito");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            //finally //opcional
+            //{
+
+            //Limpiar();
+
+            //}
+
+            else
+            {
+                MessageBox.Show("Cédula requerida");
+            }
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader leer = null;
+                leer = File.OpenText("Datos.txt");
+                string linea = "";
+                linea = leer.ReadLine();
+                //Split lo que hace es separar el caracter separador y lo acomoda para imprimirlo en el messagebox en posicion hacia abajo [1] [2] [3]
+                string[] p = linea.Split('#');
+                txtCedula.Text = p[0];
+                txtNombre.Text = p[1];
+                txtDireccion.Text = p[2];
+                txtCorreo.Text = p[3];
+                txtSalario.Text = p[4];
+                MessageBox.Show(linea);
+                leer.Close(); //cerrar el archivito
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Boolean encontrado = false;
+            try
+            {
+                StreamReader leer = null;
+                leer = File.OpenText("Datos.txt");
+                string linea = "";
+
+                while ((linea = leer.ReadLine()) != null)
+                {
+                    string[] p = linea.Split('#');
+                    if (txtCedula.Text == p[0])
+                    {
+                        txtNombre.Text = p[1];
+                        txtDireccion.Text = p[2];
+                        txtCorreo.Text = p[3];
+                        txtSalario.Text = p[4];
+                        encontrado = true;
+                        btnModificar.Enabled = true;
+                        btnBorrar.Enabled = true;
+
+                    }
+                }
+                if (encontrado == false)
+                {
+                    MessageBox.Show("Registro no encontrado");
+                    Limpiar();
+                }
+
+                leer.Close(); //cerrar el archivito
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                StreamReader leer = null;
+                StreamWriter arch = null; // se crea un nuevo archivo  
+
+                leer = File.OpenText("Datos.txt"); //se abre para leer
+                arch = File.AppendText("temp.txt");
+                string linea = "";
+
+                while ((linea = leer.ReadLine()) != null)
+                {
+
+                    string[] p = linea.Split('#');
+                    if (txtCedula.Text != p[0])
+                    {
+
+                        arch.WriteLine(linea);
+                    }
+                  
+                }
+              
+                arch.Close();
+                leer.Close();
+                File.Delete("Datos.txt");
+                File.Copy("temp.txt", "Datos.txt", true);
+                File.Delete("temp.txt");
+                Limpiar();
+                MessageBox.Show("Registro borrado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+        
+
+         
+    
+}
